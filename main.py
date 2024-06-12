9cf4f71b90c62bae962ae5abb2db5ccb6bef2e23
kubula316 <kubula316@o2.pl>
2024-06-12 20:49:38 +0200

    v0.001

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ path = os.path.join(os.getcwd(), 'images')
 file_names = os.listdir(path)
 LIGHTGREEN = pygame.color.THECOLORS['lightgreen']
 BACKGROUND = pygame.image.load(os.path.join(path, 'Background.png')).convert()
-ATTACKTIME = pygame.USEREVENT +1
-
+P1ATTACK = pygame.USEREVENT + 1
+P2ATTACK = pygame.USEREVENT + 2
+pygame.time.set_timer(P1ATTACK, 1)
+pygame.time.set_timer(P2ATTACK, 1)
 
 file_names.remove('Background.png')
 IMAGES = {}
@@ -31,23 +33,32 @@ class Player(pygame.sprite.Sprite):
         self.score = 0
         self.CanAttack = False
         self.FailedAttack = False
+        self.IsAttacking = False
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
     def Attack(self):
         if exclamation.AllowAttack:
-            print("Atak UDANY")
-            # Animacja ataku
             if self.image == IMAGES['PLAYER2']:
-                print("P2 ATTACKED")
                 self.image = IMAGES['PLAYER2ATTACK']
+                if player.lives > 1:
+                    player.lives -= 1
+                    #aniamcja ataku z cofnieciem
+                if player.lives == 1:
+                    print("koniec gry WYGRYWA P2")
+                    # aniamcja ataku bez cofnieciem
+                    #zakonczenie gry
             elif self.image == IMAGES['PLAYER']:
-                print("P1 ATTACKED")
                 self.image = IMAGES['PLAYER1ATTACK']
+                player2.lives -= 1
+                exclamation.AllowAttack = False
+                player2.image = IMAGES['PLAYER2DAMAGE']
+                player.IsAttacking = True
 
         else:
             print("Atak Zjeaby")
+
 
 
 
@@ -85,21 +96,15 @@ class Exclamation():
         current_time2 = pygame.time.get_ticks()
         if self.start_time2 is None:
             self.start_time2 = current_time2
-            self.random_interval = random.randint(3000, 6000)  # Generate new random interval
+            self.random_interval = random.randint(2000, 4000)  # Generate new random interval
             print(self.random_interval/1000)
         if current_time2 - self.start_time2 >= self.random_interval:
             # Perform the action
             self.AllowAttack = True
             # Reset the timer
             self.start_time2 = current_time2
-            self.random_interval = random.randint(3000, 6000)  # Generate new random interval for next action
+            self.random_interval = random.randint(2000, 4000)  # Generate new random interval for next action
             print(self.random_interval/1000)
-
-
-
-
-
-
 
 
 # konkretyzacja obiektów
@@ -121,12 +126,13 @@ while window_open:
                 window_open = False
             if event.key == pygame.K_RIGHT and event.key == pygame.K_LEFT:
                 print("Remis")
+            #ATAKI GRACZY
             if event.key == pygame.K_RIGHT:
                 player2.Attack()
 
-
             if event.key == pygame.K_LEFT:
                 player.Attack()
+
 
 
         if event.type == pygame.QUIT:
@@ -136,6 +142,27 @@ while window_open:
 
     # rysowanie i aktualizacja obiektów
     exclamation.StartTimer()
+    if event.type == P1ATTACK:
+        if player.IsAttacking:
+            if player.rect.x < 600:
+                player.rect.x += 60
+            if player.rect.y > 200:
+                player.rect.y -= 20
+            if player.rect.x > 600 and player.rect.y < 200:
+                pygame.time.delay(500)
+                player.image = IMAGES['PLAYER']
+                player2.image = IMAGES["PLAYER2"]
+                player.rect.center = 550, 500
+                player.IsAttacking = False
+
+
+
+
+
+
+
+
+
     player.draw(screen)
     player2.draw(screen)
     exclamation.CheckAttack()
