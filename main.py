558f29b3489ede15685e1ac6cfeaf248fb6b09c4
kubula316 <kubula316@o2.pl>
2024-06-14 13:14:36 +0200

    v0.02

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ clock = pygame.time.Clock()
 path = os.path.join(os.getcwd(), 'images')
 file_names = os.listdir(path)
 LIGHTGREEN = pygame.color.THECOLORS['lightgreen']
+RED = pygame.color.THECOLORS['red']
 BACKGROUND = pygame.image.load(os.path.join(path, 'Background.png')).convert()
-MENU = pygame.image.load(os.path.join(path, 'Background.png')).convert()
+MENU =pygame.image.load(os.path.join(path, 'MENU.png')).convert()
 Failed_Attack_P1 = pygame.USEREVENT + 1
 Failed_Attack_P2 = pygame.USEREVENT + 2
 
@@ -131,16 +132,56 @@ class Level():
         self.IntroWasPlayed = False
         self.GameHasEnded = False
         self.MenuActive = False
+        self.P1Won = False
+        self.P2Won = False
 
     def draw(self):
         screen.blit(BACKGROUND, [0, 0])
         if not self.MenuActive:
+            player.draw(screen)
+            player2.draw(screen)
+            cross.draw(screen)
+            cross2.draw(screen)
             for i in range(self.player1.lives):
                 screen.blit(IMAGES['HEARTH2'], (20 + i * 120, 750))
             for i in range(self.player2.lives):
                 screen.blit(IMAGES['HEARTH2'], (1460 - i * 120, 20))
         else:
             screen.blit(MENU, [0, 0])
+
+    def ResetGame(self):
+        self.player1.lives = 3
+        self.player1.CanAttack = False
+        self.player1.FailedAttack = False
+        self.player1.IsAttacking = False
+        self.player2.lives = 3
+        self.player2.CanAttack = False
+        self.player2.FailedAttack = False
+        self.player2.IsAttacking = False
+        self.IntroWasPlayed = False
+        self.GameHasEnded = True
+        self.MenuActive = True
+        self.P1Won = False
+        self.P2Won = False
+        player.rect.center = -150, 500
+        player2.rect.center = 1750, 300
+        player.image = IMAGES['PLAYER']
+        player2.image = IMAGES["PLAYER2"]
+class Text:
+    def __init__(self, text, text_color, pc_x, pc_y, font_size = 36, font_type = None):
+        self.text = str(text)
+        self.text_color = text_color
+        self.font_size = font_size
+        self.font_type = font_type
+        self.font = pygame.font.SysFont(self.font_type, self.font_size)
+        self.pc_x = pc_x
+        self.pc_y = pc_y
+
+    def draw(self, surface):
+        self.image = self.font.render(self.text, True, self.text_color)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pc_x, self.pc_y
+        surface.blit(self.image, self.rect)
 
 
 # konkretyzacja obiektów
@@ -150,85 +191,99 @@ level = Level(player, player2)
 exclamation = Exclamation(IMAGES['EXCLAMATION'], 800, 400)
 cross = Cross(IMAGES['CROSS'], 550, 500)
 cross2 = Cross(IMAGES['CROSS'], 1050, 300)
+P1_win_text = Text("P1 WINS!", RED, *screen.get_rect().center, font_size=250, font_type="Ink Free")
 
 # pętla gry
 window_open = True
 
 while window_open:
-
-    # pętla zdarzeń
     level.draw()
-    if not level.IntroWasPlayed:
-        if player.rect.x < 550 - 190 and player2.rect.x > 1050 - 190:
-            player.rect.x += 10
-            player2.rect.x -= 10
-        else:
-            level.IntroWasPlayed = True
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                window_open = False
-            if event.key == pygame.K_RIGHT and event.key == pygame.K_LEFT:
-                print("Remis")
-            # ATAKI GRACZY
-            if event.key == pygame.K_RIGHT:
-                player2.Attack()
-            if event.key == pygame.K_LEFT:
-                player.Attack()
-
-        if event.type == pygame.QUIT:
-            window_open = False
-        if event.type == Failed_Attack_P1:
-            player.FailedAttack = False
-            cross.drawing = False
-        if event.type == Failed_Attack_P2:
-            player2.FailedAttack = False
-            cross2.drawing = False
-
-    exclamation.startTimer()
-
-    if player.IsAttacking:
-        if player.rect.x < 600:
-            player.rect.x += 60
-        if player.rect.y > 200:
-            player.rect.y -= 20
-        if player.rect.x > 600 and player.rect.y < 200:
-            pygame.time.delay(500)
-            if player2.lives > 0:
-                player.image = IMAGES['PLAYER']
-                player2.image = IMAGES["PLAYER2"]
-                player.rect.center = 550, 500
-                player2.FailedAttack = False
-                cross2.drawing = False
-                player.IsAttacking = False
+    # pętla zdarzeń
+    if not level.MenuActive:
+        if not level.IntroWasPlayed:
+            if player.rect.x < 550 - 190 and player2.rect.x > 1050 - 190:
+                player.rect.x += 10
+                player2.rect.x -= 10
             else:
-                print("P1 wygrywa!")
-                level.GameHasEnded = True
+                level.IntroWasPlayed = True
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    level.MenuActive = True
+                if event.key == pygame.K_RIGHT and event.key == pygame.K_LEFT:
+                    print("Remis")
+                # ATAKI GRACZY
+                if event.key == pygame.K_RIGHT:
+                    player2.Attack()
+                if event.key == pygame.K_LEFT:
+                    player.Attack()
 
-    if player2.IsAttacking:
-        if player2.rect.x > 600:
-            player2.rect.x -= 60
-        if player2.rect.y < 400:
-            player2.rect.y += 20
-        if player2.rect.x < 600 and player.rect.y > 200:
-            pygame.time.delay(500)
-            if player.lives > 0:
-                player2.image = IMAGES['PLAYER2']
-                player.image = IMAGES["PLAYER"]
-                player2.rect.center = 1050, 300
+            if event.type == pygame.QUIT:
+                window_open = False
+            if event.type == Failed_Attack_P1:
                 player.FailedAttack = False
                 cross.drawing = False
-                player2.IsAttacking = False
-            else:
-                print("P2 wygrywa!")
-                level.GameHasEnded = True
+            if event.type == Failed_Attack_P2:
+                player2.FailedAttack = False
+                cross2.drawing = False
+
+        exclamation.startTimer()
+
+        if player.IsAttacking:
+            if player.rect.x < 600:
+                player.rect.x += 60
+            if player.rect.y > 200:
+                player.rect.y -= 20
+            if player.rect.x > 600 and player.rect.y < 200:
+
+                if player2.lives > 0:
+                    pygame.time.delay(500)
+                    player.image = IMAGES['PLAYER']
+                    player2.image = IMAGES["PLAYER2"]
+                    player.rect.center = 550, 500
+                    player2.FailedAttack = False
+                    cross2.drawing = False
+                    player.IsAttacking = False
+                else:
+                    print("P1 wygrywa!")
+                    level.GameHasEnded = True
+                    level.P1Won = True
+                    P1_win_text.draw(screen)
+                    pygame.display.flip()
+                    with open('Match_history.txt', 'a') as file:
+                        file.write("P1Wins\n")
+                    pygame.time.delay(5000)
+                    level.MenuActive = True
+                    level.ResetGame()
+
+        if player2.IsAttacking:
+            if player2.rect.x > 600:
+                player2.rect.x -= 60
+            if player2.rect.y < 400:
+                player2.rect.y += 20
+            if player2.rect.x < 600 and player.rect.y > 200:
+                pygame.time.delay(500)
+                if player.lives > 0:
+                    player2.image = IMAGES['PLAYER2']
+                    player.image = IMAGES["PLAYER"]
+                    player2.rect.center = 1050, 300
+                    player.FailedAttack = False
+                    cross.drawing = False
+                    player2.IsAttacking = False
+                else:
+                    print("P2 wygrywa!")
+                    level.GameHasEnded = True
+        exclamation.checkAttack()
+    else:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    window_open = False
+                if event.key == pygame.K_UP:
+                    level.MenuActive = False
 
     # rysowanie i aktualizacja obiektów
-    player.draw(screen)
-    player2.draw(screen)
-    exclamation.checkAttack()
-    cross.draw(screen)
-    cross2.draw(screen)
+
 
     # aktualizacja okna gry
     pygame.display.flip()
